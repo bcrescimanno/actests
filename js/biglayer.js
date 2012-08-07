2c7049e7abe604d51bba2b07fd6e3b6b099c7967
Brian Crescimanno <brian.crescimanno@gmail.com>
2012-08-07 14:28:49 -0700

    Add a 720p big layer test

--- js/biglayer.js
+++ js/biglayer.js
@@ -1,41 +1,40 @@
-var CONFIG = {
-    baseOffset: 10,
-    width: 325,
-    baseTop: 0,
-    height: 500,
-    containers: 4,
-    currentTop: 0
-};
+/*global CONFIG */
 
-function redraw () {
-    var containers = document.getElementsByClassName("container"),
+(function(CONFIG, exports) {
+    function redraw () {
+        var containers = document.getElementsByClassName("container"),
         numContainers = containers.length,
         topIndex = CONFIG.currentTop,
         offsetFromTop,
         j,
         y;
 
-    for (j = 0; j < numContainers; j++) {
-        offsetFromTop = Math.abs(j - topIndex);
-        y = CONFIG.baseTop + (offsetFromTop * CONFIG.height);
-        containers[j].style.webkitTransform = 
-            "translate3d(0, " + y + "px, 0)";
-    }
-}
-
-function handleKeyInput (e) {
-    e.preventDefault();
-    if (CONFIG.currentTop === (CONFIG.containers - 1)) {
-        CONFIG.currentTop = 0;
-    } else {
-        CONFIG.currentTop += 1;
+        for (j = 0; j < numContainers; j++) {
+            offsetFromTop = 0;
+            if (j > topIndex) {
+                offsetFromTop = j - topIndex;
+            } else if (j < topIndex) {
+                offsetFromTop = CONFIG.containers + (j - topIndex); 
+            }
+            y = CONFIG.baseTop + (offsetFromTop * CONFIG.height);
+            containers[j].style.webkitTransform = 
+                "translate3d(0, " + y + "px, 0)";
+        }
     }
 
-    redraw();
-}
+    function handleKeyInput (e) {
+        e.preventDefault();
+        if (CONFIG.currentTop === (CONFIG.containers - 1)) {
+            CONFIG.currentTop = 0;
+        } else {
+            CONFIG.currentTop += 1;
+        }
 
-function setup () {
-    var containers = document.getElementsByClassName("container"),
+        redraw();
+    }
+
+    function setup () {
+        var containers = document.getElementsByClassName("container"),
         numContainers = containers.length,
         topIndex = CONFIG.currentTop,
         curContainer,
@@ -47,24 +46,28 @@ function setup () {
         y,
         numItems;
 
-    for (j = 0; j < numContainers; j++) {
-        items = containers[j].getElementsByClassName("item");
+        for (j = 0; j < numContainers; j++) {
+            items = containers[j].getElementsByClassName("item");
 
-        offsetFromTop = Math.abs(j - topIndex);
-        y = CONFIG.baseTop + (offsetFromTop * CONFIG.height);
-        containers[j].style.webkitTransform = 
-            "translate3d(0, " + y + "px, 0)";
+            offsetFromTop = Math.abs(j - topIndex);
+            y = CONFIG.baseTop + (offsetFromTop * CONFIG.height);
+            containers[j].style.webkitTransform = 
+                "translate3d(0, " + y + "px, 0)";
 
-        for (i = 0, numItems = items.length; i < numItems; i++) {
-            x = CONFIG.baseOffset + (i * CONFIG.width);
-            items[i].style.webkitTransform = 
-                "translate(" + x + "px, 0)";
+            for (i = 0, numItems = items.length; i < numItems; i++) {
+                x = CONFIG.baseOffset + (i * CONFIG.width);
+                items[i].style.webkitTransform = 
+                    "translate(" + x + "px, 0)";
+            }
+
+            curContainer = containers[j];
         }
 
-        curContainer = containers[j];
+        document.addEventListener("keydown", handleKeyInput, true);
+
     }
 
-    document.addEventListener("keydown", handleKeyInput, true);
+    exports.setup = setup;
 
-}
 
+})(CONFIG, window);
