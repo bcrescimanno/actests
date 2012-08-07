f15113b1ed1e9c579c8e8d089fb5c87785325f47
Brian Crescimanno <brian.crescimanno@gmail.com>
2012-08-07 14:40:26 -0700

    adding 720p container of containers test

--- js/containerofcontainer.js
+++ js/containerofcontainer.js
@@ -1,37 +1,29 @@
-var CONFIG = {
-    baseOffset: 10,
-    width: 325,
-    baseTop: 0,
-    height: 500,
-    containers: 4,
-    currentTop: 0,
-    topOffset: 0,
-    topOffsetBy: 0
-};
+/*global CONFIG */
 
-function handleKeyInput(e) {
-    var move = document.getElementById("movingWrapper"),
+(function(CONFIG, exports) {
+    function handleKeyInput(e) {
+        var move = document.getElementById("movingWrapper"),
         containers = document.getElementsByClassName("container"),
         newTop = CONFIG.topOffset - CONFIG.height,
         newOffset;
 
-    e.preventDefault();
+        e.preventDefault();
 
-    newOffset = (CONFIG.topOffsetBy * CONFIG.height) + (CONFIG.height * CONFIG.containers);
-    CONFIG.topOffsetBy++;
+        newOffset = (CONFIG.topOffsetBy * CONFIG.height) + (CONFIG.height * CONFIG.containers);
+        CONFIG.topOffsetBy++;
 
-    move.style.webkitTransform = 
-        "translate3d(0, " + newTop + "px, 0)";
+        move.style.webkitTransform = 
+            "translate3d(0, " + newTop + "px, 0)";
 
-    containers[CONFIG.currentTop].style.webkitTransform =
-        "translate3d(0, " + newOffset + "px, 0)";
+        containers[CONFIG.currentTop].style.webkitTransform =
+            "translate3d(0, " + newOffset + "px, 0)";
 
-    CONFIG.topOffset = newTop;
-    CONFIG.currentTop = (CONFIG.currentTop === 3) ? 0 : CONFIG.currentTop + 1;
-}
+        CONFIG.topOffset = newTop;
+        CONFIG.currentTop = (CONFIG.currentTop === 3) ? 0 : CONFIG.currentTop + 1;
+    }
 
-function setup () {
-    var containers = document.getElementsByClassName("container"),
+    function setup () {
+        var containers = document.getElementsByClassName("container"),
         numContainers = containers.length,
         topIndex = CONFIG.currentTop,
         offsetFromTop,
@@ -42,21 +34,26 @@ function setup () {
         y,
         numItems;
 
-    for (j = 0; j < numContainers; j++) {
-        items = containers[j].getElementsByClassName("item");
+        for (j = 0; j < numContainers; j++) {
+            items = containers[j].getElementsByClassName("item");
 
-        offsetFromTop = Math.abs(j - topIndex);
-        y = CONFIG.baseTop + (offsetFromTop * CONFIG.height);
-        containers[j].style.webkitTransform = 
-            "translate3d(0, " + y + "px, 0)";
+            offsetFromTop = Math.abs(j - topIndex);
+            y = CONFIG.baseTop + (offsetFromTop * CONFIG.height);
+            containers[j].style.webkitTransform = 
+                "translate3d(0, " + y + "px, 0)";
 
-        for (i = 0, numItems = items.length; i < numItems; i++) {
-            x = CONFIG.baseOffset + (i * CONFIG.width);
-            items[i].style.webkitTransform = 
-                "translate3d(" + x + "px, 0, 0)";
+            for (i = 0, numItems = items.length; i < numItems; i++) {
+                x = CONFIG.baseOffset + (i * CONFIG.width);
+                items[i].style.webkitTransform = 
+                    "translate3d(" + x + "px, 0, 0)";
+            }
         }
+
+        document.addEventListener("keydown", handleKeyInput, true);
+
     }
 
-    document.addEventListener("keydown", handleKeyInput, true);
+    exports.setup = setup;
 
-}
+})(CONFIG, window);
+
